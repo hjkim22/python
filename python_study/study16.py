dcd3cf0fabd9e76214fc03e2f2ba498ac4365309
hjkim22 <hjhjgudwls1022@gmail.com>
2023-12-28 23:54:53 +0900

    feat : bool

---
+++ python_study/study16.py
@@ -0,0 +1,20 @@
+# bool
+# 주의할점 True x true / False x false
+
+# 부등호
+print(3>1)
+print(3==1)
+print(3==3)
+
+print(3 != 33)
+
+# 문자열 비교
+print("python" == "Python")
+print("python" == "python")
+print("python" != "python")
+
+print(3.0 is 3) # is는 객체의 타입을 비교
+print(3.0 == 3)
+
+print(id(3.0))
+print(id(3))
