ff9ffba0f4b96bbe486c73392ef1f029d40677dd
hjkim22 <hjhjgudwls1022@gmail.com>
2023-12-29 00:03:40 +0900

    feat : and / or

---
+++ python_study/study17.py
@@ -0,0 +1,17 @@
+# 논리연산자
+# and
+# True : 1 / False : 0
+# and는 * 라고 생각하면 편함
+print(True and True) # 1 * 1 = 1 True
+print(True and False) # 1 * 0 = 0 False
+print(False and True) # 0 * 1 = 0 False
+print(False and False) # 0 * 0 = 0 False
+
+# 논리연산자
+# or
+# True : 1 / False : 0
+# or은 + 라고 생각하면 편함
+print(True or True) # 1 + 1 = 2 True
+print(True or False) # 1 + 0 = 1 True
+print(False or True) # 0 + 1 = 1 True
+print(False or False) # 0 + 0 = 0 False
