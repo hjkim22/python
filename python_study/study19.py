70845cbd3d742a92adf9851800d14fc08fcb4d81
hjkim22 <hjhjgudwls1022@gmail.com>
2023-12-29 00:17:01 +0900

    feat : 단락평가

---
+++ python_study/study19.py
@@ -0,0 +1,9 @@
+# 문자열 / True, False
+print(bool("카톡카톡"))
+
+# 단락 평가
+print(False and True) # "False and"가 나오면 어차피 False가 나옴
+print(False and "카톡") # 단락평가 : 뒤를 보지않음
+
+print(True or False) # "True or"이 나오면 어차피 True가 나옴
+print(True or "카톡")
