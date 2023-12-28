9482494c64a3d748c23ec9c245e7a7a342fbe81f
hjkim22 <hjhjgudwls1022@gmail.com>
2023-12-28 17:01:56 +0900

    feat : 시퀀스 자료형 / index

---
+++ python_study/study10.py
@@ -0,0 +1,44 @@
+# 자료형 종류
+# 1. int(integer) : 정수
+# 2. float(float) : 실수
+# 3. str(string) : 문자
+# 4. bool(bool, boolean) : 참 또는 거짓, true 또는 false, 1 또는 0
+###############
+# 5. list
+# 6. tuple
+# 7. dict
+# 8. set
+
+이름 = "김형진"
+나이 = 27
+키 = 183.5
+소속 = "경기, 취준생 백수"
+
+### print(type(이름))
+### print(type(나이))
+### print(type(키))
+### print(type(소속))
+
+### print(float(5))
+### print(int(5.0))
+### print("5", "5.0")
+### print(type("5"), type("5.0"))
+
+# str : 문자열
+# 특징 : 시퀀스 자료형
+# 시퀀스랑? 요소들이 연속으로 이어진 자료형
+# 값 하나하나를 요소라고 한다.
+
+# 0 인덱스  
+### print(소속)
+### print(소속[0])
+### print(소속[1])
+### print(소속[2])
+### print(소속[3])
+### print(소속[4])
+### print(소속[5])
+### print(소속[6])
+
+### print("백" in 소속)
+### print("3" in 소속)
+
