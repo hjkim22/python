0ed0b16471e9a0a504f3ac828abc3f236c736de1
hjkim22 <hjhjgudwls1022@gmail.com>
2023-12-28 17:43:44 +0900

    feat : study9~13 복습

---
+++ python_study/study14.py
@@ -0,0 +1,32 @@
+
+# study 9~13 복습
+
+라라랜드 = "사람들은 다른 사람들의 열정에 끌리게 되어있어 자신이 잊은 걸 상기시켜 주니까"
+
+
+print(type(라라랜드))
+
+print(라라랜드)
+print(라라랜드[0])
+print(라라랜드[2])
+print(라라랜드[4])
+print(라라랜드[6])
+
+print("들" in 라라랜드)
+print("똥" in 라라랜드)
+
+print(len(라라랜드))
+
+text_1 = 라라랜드[0:4]
+text_2 = 라라랜드[13:16]
+text_3 = 라라랜드[40:43]
+print(f'{text_1} {text_2} {text_3}')
+
+print(라라랜드[::-1])
+
+print(라라랜드[::2])
+print(라라랜드[4::2])
+
+print(라라랜드.count("사"))
+
+print(라라랜드.index("까"))
