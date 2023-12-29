96d1a77ff5ffc7a2b57ac5a05087b1f252541bc1
hjhj <hjhjgudwls@gmail.com>
2023-12-29 16:00:50 +0900

    feat : input /  형변환

---
+++ python_study/study22.py
@@ -0,0 +1,14 @@
+# 입력과 출력
+
+# 입력 : input()
+# 출력 : print()
+
+# int(), float(), str(), bool()
+
+정수 = input()
+정수 = int(정수)
+print(정수, type(정수))
+
+실수 = input()
+실수 = float(실수)
+print(실수, type(실수))
