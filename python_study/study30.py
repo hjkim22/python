1e4aa1bbc0ab80e68ee405d186f4865d42ef9eef
hjhj <hjhjgudwls@gmail.com>
2024-01-02 16:14:41 +0900

    feat : list

---
+++ python_study/study30.py
@@ -0,0 +1,23 @@
+리스트 = ["이","렇","게","만","들","어"]
+print(리스트)
+
+
+리스트2 = list()
+print(리스트2)
+
+# range? 연속된 숫자를 생산하는 기능
+range(10)
+
+# list(range(시작, 끝))
+리스트_매직 = list(range(0,20))
+print(리스트_매직)
+
+#list(range(시작, 끝, 증가))
+리스트_매직2 = list(range(0,20,2))
+print(리스트_매직2)
+
+리스트_매직3 = list(range(20, 0, -1))
+print(리스트_매직3)
+
+리스트_매직4 = list(range(20, 0))
+print(리스트_매직4)
