10c786f2029b66fbf21e9ce9b1a616c56bfbf66f
hjhj <hjhjgudwls@gmail.com>
2024-01-04 10:46:57 +0900

    feat : list

---
+++ python_test2/list_3.py
@@ -0,0 +1,12 @@
+# [언어1]과 [언어2] 리스트가 있을 때 언어1과 언어2의 원소를 모두 갖고 있는 [언어모음]이라는 list를 만들어주세요
+
+# lang1 = ["C", "C++", "JAVA"]
+# lang2 = ["Python", "Go", "C#"]
+
+# 실행 예: 언어들 ['C', 'C++', 'JAVA', 'Python', 'Go', 'C#']
+
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+
+언어들 = lang1 + lang2
+print(f'언어들 {언어들}')
---
+++ python_test2/list_4.py
@@ -0,0 +1,10 @@
+# 다음 리스트에서 최댓값과 최솟값을 출력해주세요.
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+
+nums = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+
+num_max = max(nums)
+num_min = min(nums)
+
+print(f'최댓값:{num_max} / 최솟값:{num_min}')
