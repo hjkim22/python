03e2900f2cc918038e96eaad73de248de6583911
hjhj <hjhjgudwls@gmail.com>
2024-01-05 02:12:02 +0900

    feat : 파일 입출력

---
+++ python_study/study80.py
@@ -0,0 +1,11 @@
+# 파일 입출력
+
+# 웹 > 크롤링 > 데이터 베이스 > 1차 클렌징 > 출력 후 담당자 전달 > 2차 클렌징 > 유용한 데이터 셋 > csv 데이터베이스 입력
+
+# 파일 드라이브 단축키
+
+# r - 읽기
+# w - 쓰기 (주의 : 덮어쓰기)
+# a - 추가
+
+# https://colab.research.google.com/drive/1xvDr6WDixMBzuWSnpIBOOgNaNVPVOdCq#scrollTo=ilx_Kl_PcQkI
