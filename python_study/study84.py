3783e0b6c65e43b908b0af3ebe63f43ce7da6a0d
hjhj <hjhjgudwls@gmail.com>
2024-01-10 18:17:25 +0900

    feat : while

---
+++ python_study/study84.py
@@ -0,0 +1,22 @@
+# # customer = "토르"
+# # index = 5
+# # while index >= 1:
+# #     print(f"{customer}, {index}번 준비")
+# #     index -= 1
+# #     if index == 0:
+# #         print("폐기")
+
+# customer = "ironman"
+# index = 1
+# while True:
+#     print(f"{customer}, 호출 {index}회")
+#     index += 1
+
+customer = "iron"
+person = "Unkown"
+
+while person != customer:
+    print(f"{customer} 준비됐어")
+    person = input("이름 뭐냐")
+
+
