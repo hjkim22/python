063652d7685f8d949bbc0174339c60836ccdc467
hjhj <hjhjgudwls@gmail.com>
2024-01-10 18:44:08 +0900

    feat : for / if 예제

---
+++ python_study/study87.py
@@ -0,0 +1,11 @@
+from random import *
+
+count = 0
+for i in range(1, 51):
+    time = randrange(5, 51)
+    if 5 <= time <= 15:
+        print(f'[0] {i}번째 손님 (소요시간 : {time}분)')
+        count += 1
+    else:
+        print(f'[ ] {i}번째 손님 (소요시간 : {time}분)')
+print(f'총 탑승 승객 : {count}명')
