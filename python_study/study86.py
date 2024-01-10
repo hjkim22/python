05dfbcf118c6737855fcd43ea30abaa19283694e
hjhj <hjhjgudwls@gmail.com>
2024-01-10 18:31:24 +0900

    feat : 한줄 for

---
+++ python_study/study86.py
@@ -0,0 +1,18 @@
+# 한줄 for
+
+# number = [1, 2, 3, 4, 5]
+# print(number)
+# number =[i+100 for i in number]
+# print(number)
+
+
+# 길이로 변환
+# customer = ["iron man", "Thor", "i am groot"]
+# customer = [len(i) for i in customer]
+# print(customer)
+
+
+# 대문자로 변환
+customer = ["iron man", "Thor", "i am groot"]
+customer = [i.upper() for i in customer]
+print(customer)
