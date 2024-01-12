7e467b1eebdcc8b29f8d511036f79ed57de093fa
hjhj <hjhjgudwls@gmail.com>
2024-01-12 18:03:56 +0900

    feat : class / 상속

---
+++ python_study2/study2-8.py
@@ -0,0 +1,34 @@
+# class
+# 상속
+
+# 일반 유닛
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+# 공격 유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{} : {} 방향으로 적군 공격 [공격력 {}]".format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{} : {} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{} : 현재 체력 {}".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{} : 파괴되었습니다.".format(self.name))
+
+# 메딕 : 의무병
+
+# 파이어뱃 : 공격 유닛, 화염방사기
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
