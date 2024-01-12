39e0d7d9b8da15d3109419597aba511ab2f72f6a
hjhj <hjhjgudwls@gmail.com>
2024-01-12 17:16:44 +0900

    feat : class

---
+++ python_study2/study2-5.py
@@ -0,0 +1,13 @@
+# class
+
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{} 유닛 생성.".format(self.name))
+        print("체력 {}, 공격력 {}".format(self.hp, self.damage))
+
+marine1 = Unit("마린", "40", "5")
+marine2 = Unit("마린", "40", "5")
+tank = Unit("탱크", "150", "35")
